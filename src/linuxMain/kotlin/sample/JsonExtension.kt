dbc4cfe54f0d1ede9adbc26d04cb5ad92d8e5e18
Kavan Mevada <kavanmevadad@gmail.com>
2019-10-22 10:57:16 +0530

    libcurl + glib-json intergration

---
+++ src/linuxMain/kotlin/sample/JsonExtension.kt
@@ -0,0 +1,32 @@
+package sample
+
+import glibjson.*
+import kotlinx.cinterop.*
+
+
+fun JSON(string: String): CPointer<JsonNode>? {
+    val parser = json_parser_new()
+    val bool = json_parser_load_from_data(parser, string, string.length.convert(), null)
+    return json_parser_get_root(parser)
+}
+
+
+
+fun CPointer<JsonNode>?.get() = json_node_get_parent(this)
+
+fun  CPointer<JsonNode>?.asArray() = json_node_get_array(this)
+fun CPointer<JsonNode>?.asObject() = json_node_get_object(this)
+
+
+
+
+fun CPointer<JsonArray>.getObject(index: Int) = json_array_get_object_element(this, index.toUInt())
+fun CPointer<JsonArray>.getArray(index: Int) = json_array_get_array_element(this, index.toUInt())
+
+fun CPointer<JsonObject>.getObject(key: String) = json_object_get_object_member(this, key)
+fun CPointer<JsonObject>.getArray(key: String) = json_object_get_array_member(this, key)
+
+fun CPointer<JsonObject>.getInt(key: String) = json_object_get_int_member(this, key)
+fun CPointer<JsonObject>.getDouble(key: String) = json_object_get_double_member(this, key)
+fun CPointer<JsonObject>.getString(key: String) = json_object_get_string_member(this, key)
+fun CPointer<JsonObject>.getBoolean(key: String) = json_object_get_boolean_member(this, key)
--- src/linuxMain/kotlin/sample/main.kt
+++ src/linuxMain/kotlin/sample/main.kt
@@ -1,8 +1,14 @@
 package sample
 
+import glibjson.*
 import glibresources.*
+import glibresources.FALSE
+import glibresources.TRUE
+import glibresources.g_idle_add
+import glibresources.g_thread_new
 import kotlinx.cinterop.*
 import kotlinx.coroutines.*
+import libcurl.*
 import libgtk3.*
 import libgtk3.GConnectFlags
 import libgtk3.G_APPLICATION_FLAGS_NONE
@@ -12,6 +18,7 @@ import libgtk3.g_signal_connect_data
 import libgtk3.gdouble
 import libgtk3.gpointer
 import libgtk3.gulong
+import platform.posix.size_t
 import platform.posix.sleep
 
 
@@ -22,21 +29,24 @@ fun <F : CFunction<*>> g_signal_connect(obj: CPointer<*>, actionName: String, ac
 var progress: gdouble = 0.00
 var progressBar : CPointer<GtkWidget>? = null
 var btStart : CPointer<GtkWidget>? = null
+var label : CPointer<GtkWidget>? = null
+
+
 
 fun incrementProgressBar() {
-    g_print("Inside thread ... ")
-    var i = 0.05
-    while (progress < 1.0) {
+    println("Inside thread ... ")
+    var i = 0.00
+    while (progress < 1.00) {
         g_idle_add(staticCFunction(::incrementGtkProgressBar).reinterpret(), null)
-        progress = progress + 0.10
-        sleep(1)
+        progress += 0.10
+        sleep(1.toUInt())
     }
-    g_print("Thread completed...")
+    println("Thread completed...")
     g_idle_add(staticCFunction(::resetGtkProgressBar).reinterpret(), null)
 }
 
 fun incrementGtkProgressBar(): Int {
-    gtk_progress_bar_set_fraction ( progressBar!!.reinterpret() , progress ) ;
+    gtk_progress_bar_set_fraction ( progressBar!!.reinterpret() , progress )
     return FALSE
 }
 
@@ -67,15 +77,24 @@ fun activate(app: CPointer<GtkApplication>?, user_data: gpointer?) {
 
     val grid = gtk_grid_new()
 
+    val image = gtk_image_new_from_resource("/org/gtk/example/icon.png")
     btStart = gtk_button_new_with_label("Start")
+    label = gtk_label_new("City Name")
     progressBar = gtk_progress_bar_new()
 
-    gtk_grid_attach(grid!!.reinterpret(), btStart, 0, 0, 1, 1)
-    gtk_grid_attach(grid!!.reinterpret(), progressBar, 0, 1, 1, 1)
+    gtk_grid_attach(grid!!.reinterpret(), image, 0, 0, 1, 1)
+    gtk_grid_attach(grid!!.reinterpret(), label, 0, 1, 1, 1)
+    gtk_grid_attach(grid!!.reinterpret(), progressBar, 0, 2, 1, 1)
+    gtk_grid_attach(grid!!.reinterpret(), btStart, 0, 3, 1, 1)
 
 
 
-    val image = gtk_image_new_from_resource("/org/gtk/example/icon.png")
+    val curl = CUrl("https://www.metaweather.com/api/location/search/?query=Ahmedabad").close
+    curl?.parseJson()
+
+
+
+
 
     gtk_container_add (window.reinterpret(), grid)
 
@@ -100,15 +119,11 @@ fun gtkMain(args: Array<String>): Int {
 }
 
 
+
 fun main(args: Array<String>) {
     glibresources_get_resource()
     //main2(args)
     gtkMain(args)
-}
-
-
-suspend fun sequentialRequests() {
-
 }
 
 fun main2(args: Array<String>) = runBlocking<Unit> {
@@ -117,4 +132,85 @@ fun main2(args: Array<String>) = runBlocking<Unit> {
         delay(1_000)
         println("current thread2")
     }
+}
+
+class CUrl(url: String)  {
+    private val stableRef = StableRef.create(this)
+
+    private val curl = curl_easy_init()
+    private var body: String? = null
+
+    val write_callback = staticCFunction { buffer: CPointer<ByteVar>, size: size_t, nitems: size_t, userdata: COpaquePointer? ->
+        return@staticCFunction if (userdata != null) {
+            val data = buffer.toKString()
+            userdata.asStableRef<CUrl>().get().apply {
+                body = if (body==null) data else body + data
+            }
+            size * nitems
+        } else 0u
+    }
+
+    init {
+        curl_easy_setopt(curl, CURLOPT_URL, url)
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback)
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, stableRef.asCPointer())
+    }
+
+    val close: String?
+        get() {
+            curl_easy_perform(curl)
+            curl_easy_cleanup(curl)
+            stableRef.dispose()
+            return body
+        }
+}
+
+
+
+fun String.parseJson() {
+
+    val parser = json_parser_new()
+    val gbool = json_parser_load_from_data(parser, this, this.length.convert(), null)
+
+    if (gbool== TRUE){
+        val root = json_parser_get_root(parser)
+
+        val array = json_node_get_array(root)
+
+        val obj = json_array_get_object_element(array, 0.toUInt())
+
+        val woeid = json_object_get_int_member(obj, "woeid").toString()
+
+        gtk_label_set_text(label!!.reinterpret(), woeid)
+
+
+        val detailedInfo = CUrl( "https://www.metaweather.com/api/location/$woeid/").close
+
+        detailedInfo?.readDetailedInfo()
+    }
+
+    //println(result)
+}
+
+
+
+fun String.readDetailedInfo() {
+
+    val mainObject = JSON(this).asObject()
+
+
+    val consolidated_weather = mainObject?.getArray("consolidated_weather")
+
+    val weater_data = consolidated_weather?.getObject(0)
+
+
+
+
+    val main_temp = weater_data?.getString("main_temp").toLong()
+    val weather_state_name = weater_data?.getString("weather_state_name")?.toKString()
+    val wind_speed = weater_data?.getString("wind_speed")?.toKString()
+    val humidity = weater_data?.getString("humidity")?.toKString()
+
+
+    println("$main_temp , $weather_state_name, $wind_speed, $humidity")
 }
